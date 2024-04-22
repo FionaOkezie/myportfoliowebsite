7166e40c6b91afd353280f47646691824137a0ac
FionaOkezie <fionaokezie@gmail.com>
2024-04-22 18:15:14 -0230

    completed portfolio build

--- script.js
+++ script.js
@@ -77,3 +77,30 @@ projecttitles.forEach(projecttitle => {
         openproject.call(this, titlename);
     });
 });
+
+
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwhrdTzjiGROBbxpn4LbN86ezNcOPXBLrecF5ohLWCQCW06ZJeutH97a5skOzn4XB98DQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {msg.innerHTML = "Message sent Successfully"
+    setTimeout(function(){
+        msg.innerHTML = ""
+    },5000) 
+    form.reset()
+})
+      .catch(error => console.error('Error!', error.message))
+  })
